c53405cdb89aa34cd2ece5de9c3048b6d898f34a
Kevin Masterson <k.m.masterson@gmail.com>
2025-12-28 01:29:18 +0200

    generate build scripts from genbuild.py, and also include new COD games

---
+++ .github/build/linux/package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p package
+cd package
+rm -f *
+cp ../README.md ./
+cp ../LICENSE ./
+
+for f in COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET; do
+  cp ../bin/release-$f/x86/test_qmm_$f.so ./
+  cp ../bin/release-$f/x86_64/test_qmm_x86_64_$f.so ./
+done
+
+cd ..
---
+++ .github/build/windows/debug.bat
@@ -0,0 +1,4 @@
+for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET) do (
+    msbuild .\msvc\test_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x86
+    msbuild .\msvc\test_qmm.vcxproj /p:Configuration=Debug-%%x /p:Platform=x64
+)
