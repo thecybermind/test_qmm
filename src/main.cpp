be70c49bd55162e3b92b3652ec47e7f79c47ca43
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-15 01:51:19 +0200

    change vcxproj and genbuild to include debug info in builds

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ Created By:
 // #define TEST_BROADCAST
 // #define TEST_COMMAND
 // #define TEST_SOF2SP_GENTITY
+#define TEST_QVM
 
 #define _CRT_SECURE_NO_WARNINGS 1
 
@@ -65,9 +66,25 @@ C_DLLEXPORT void QMM_Detach() {
 }
 
 
+#ifdef TEST_QVM
+int old_die = 0;
+int new_die_id = 0;
+void new_die(gentity_t* self, gentity_t* inflictor, gentity_t* attacker, int damage, int mod) {
+	QMM_WRITEQMMLOG(PLID, QMM_VARARGS(PLID, "Oh no someone died! die(%p, %p, %p, %d, %d)\n", self, inflictor, attacker, damage, mod), QMMLOG_INFO);
+	
+	int args[] = { SETPTR(self, int), SETPTR(inflictor, int), SETPTR(attacker, int), damage, mod};
+	QMM_QVM_EXEC_FUNC(PLID, old_die, 5, args);
+}
+#endif // TEST_QVM
+
+
 C_DLLEXPORT intptr_t QMM_vmMain(intptr_t cmd, intptr_t* args) {
 	if (cmd == GAME_INIT) {
 		QMM_WRITEQMMLOG(PLID, QMM_VARARGS(PLID, "Test_QMM loaded! Game engine: %s\n", QMM_GETGAMEENGINE(PLID)), QMMLOG_INFO);
+	}
+
+	if (cmd == GAME_INIT) {
+		new_die_id = QMM_QVM_REGISTER_FUNC(PLID);
 	}
 
 #ifdef TEST_SOF2SP_GENTITY
@@ -224,12 +241,32 @@ C_DLLEXPORT intptr_t QMM_syscall(intptr_t cmd, intptr_t* args) {
 		QMM_WRITEQMMLOG(PLID, QMM_VARARGS(PLID, "LocateGameData(%p, %d, %d, %p, %d)\n", g_gents, g_numgents, g_gentsize, g_clients, g_clientsize), QMMLOG_INFO);
 	}
 
+#ifdef TEST_QVM
+	if (cmd == G_GET_USERCMD) {
+		gentity_t* ent = ENT_FROM_NUM(args[0]);
+		if (!ent)
+			QMM_RET_IGNORED(0);
+
+		// respawn
+		gclient_t* client = GETPTR(ent->client, gclient_t*);
+		if (client
+			&& (client->ps.pm_flags & PMF_RESPAWNED) == PMF_RESPAWNED
+			&& (client->ps.stats[STAT_WEAPONS] & (1 << WP_MACHINEGUN)) == (1 << WP_MACHINEGUN)
+			) {
+
+			if (!old_die)
+				old_die = (int)ent->die;
+			ent->die = (decltype(ent->die))new_die_id;
+			QMM_WRITEQMMLOG(PLID, QMM_VARARGS(PLID, "ent(%p)->die set to %d (was %d)!\n", ent, new_die_id, old_die), QMMLOG_INFO);
+		}
+	}
+#endif // TEST_QVM
+
 	QMM_RET_IGNORED(0);
 }
 
 
 C_DLLEXPORT intptr_t QMM_vmMain_Post(intptr_t cmd, intptr_t* args) {
-
 #ifdef TEST_RETURN_TEST
 	if (cmd == GAME_CLIENT_CONNECT) {
 		QMM_RET_SUPERCEDE((intptr_t)"Kicked, post");
@@ -262,3 +299,13 @@ C_DLLEXPORT void QMM_PluginMessage(plid_t from_plid, const char* message, void* 
 	QMM_WRITEQMMLOG(PLID, QMM_VARARGS(PLID, "Received plugin message \"%s\" with a %d-byte buffer", message, buflen), QMMLOG_INFO);
 #endif // TEST_BROADCAST
 }
+
+
+#ifdef TEST_QVM
+C_DLLEXPORT int QMM_QVMHandler(uint8_t* membase, int cmd, int* args) {
+	if (cmd == new_die_id) {
+		new_die(GETPTR(args[0], gentity_t*), GETPTR(args[1], gentity_t*), GETPTR(args[2], gentity_t*), args[3], args[4]);
+		return 0;
+	}
+}
+#endif // TEST_QVM
